ca2a855c6a0139e95d3053dcbe5a5f9b1f771f52
Hants Williams <hantsawilliams@gmail.com>
2022-12-15 14:04:05 -0500

    print logs

--- app/main.py
+++ app/main.py
@@ -115,10 +115,11 @@ def download_audio(user_uuid: str):
     for i in audio_files_clean:
         blob = bucket.blob('user/' + user_uuid + '/voice/' + i)
         blob.download_to_filename('app/temp/' + i)
+        print('downloaded ' + i)
 
     # use pythons built-in zip to zip all the files in app/temp as zip.zip
     with ZipFile('app/temp/zip.zip', 'w') as zipObj:
-        for folderName, filenames in os.walk('app/temp'):
+        for folderName, filenames in os.walk('app/temp/'):
             for filename in filenames:
                 filePath = os.path.join(folderName, filename)
                 zipObj.write(filePath, filename)
