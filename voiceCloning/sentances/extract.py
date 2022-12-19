b5e5e5b07fb1d29eb349272bcd20ae4d364bd1ed
hants <hantsawilliams@gmail.com>
2022-12-19 11:24:44 -0600

    updates
    added more complex examples with differential times for recording vices

--- voiceCloning/sentances/extract.py
+++ voiceCloning/sentances/extract.py
@@ -45,3 +45,30 @@ df['id'] = 'list' + (df['list']).astype(str) + '-' + 'q' + (df.index % 10 + 1).a
 # save the dataframe as sentances.csv
 df.to_csv('voiceCloning/sentances/sentances.csv', index=False)
 
+
+
+
+
+
+### load in phrasedata.csv.txt using pandas, splitting columns based on | and assign the columns names transcript and transcript_clean
+list2 = pd.read_csv('voiceCloning/sentances/phrasedata.csv.txt', sep='|', names = ['transcript', 'sentance'])
+list2 = list2.drop(['transcript'], axis=1)
+# create a new column called list and group each row by 10 and assign a number to it
+list2['list'] = list2.index // 10
+list2['list'] = list2['list'] + 200
+# create a new ID variable called senId and assign sen_ to each row starting at 1
+list2['senId'] = 'sn_' + (list2.index + 1).astype(str)
+# remove all special characters from the sentance column
+# list2['transcript_clean'] = list2['transcript_clean'].str.replace('[^\w\s]','')
+# create a new column that counts to 10 then resets to 1
+list2['userFieldName'] = 'q' + (list2.index % 10 + 1).astype(str)
+list2['id'] = 'list' + (list2['list']).astype(str) + '-' + 'q' + (list2.index % 10 + 1).astype(str)
+
+# save the dataframe as sentances.csv
+list2.to_csv('voiceCloning/sentances/sentances2.csv', index=False)
+
+
+### merge together the two dataframes
+df = pd.concat([df, list2], ignore_index=True)
+### save the dataframe as sentances.csv
+df.to_csv('voiceCloning/sentances/sentances3.csv', index=False)
